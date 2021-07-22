beca8fcd17a0bbd11449727cf333bbfca9cf0ede
snahid <nahidchawdhury77@gmail.com>
2021-07-22 22:03:12 +0600

    add navbar fixed

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,43 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
+
+ $(document).ready(function(){
+    $("#barguricon").click(function(){
+        if( $('#menus').css({top: "-200%"})){
+            $('#menus').css({
+                top: "100%"
+            })
+           $("#barguricon").css({
+               display: "none"
+           });
+           $("#bargurhide").css({
+            display: "block"
+        });
+        }
+    });
+
+    $("#bargurhide").click(function(){
+        if( $('#menus').css({top: "100%"})){
+            $('#menus').css({
+                top: "-200%"
+            })
+            $("#barguricon").css({
+                display: "block"
+            });
+            $("#bargurhide").css({
+             display: "none"
+         });
+        }
+    });
+
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() >= 100) {
+            $('.navbar-menu-wrapper').addClass('navbar-fixed');
+        } else {
+            $('.navbar-menu-wrapper').removeClass('navbar-fixed');
+        }
+    });
+ });
